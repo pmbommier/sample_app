232cccef00c49407042ce92768567b9d4544164d
Pierre-Marie Bommier <pm.bommier@gmail.com>
2012-10-11 17:06:47 +0200

    before 3.3.3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,28 +4,59 @@ describe "Static Pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', text: 'Sample App')
     end
   end
+end
+
+describe "static_pages" do
+	describe "A Title Test" do
+		it "should have the right title" do
+	  		visit '/static_pages/home'
+  			page.should have_selector('title',
+                    text: " | Home")
+  		end
+  	end
 end
 
 describe "Static Pages" do
 
 	describe "Help Page" do
-		it "souhld have the content 'Help'" do
+		it "souhld have the h1 'Help'" do
 			visit '/static_pages/help'
-			page.should have_content('help')
+			page.should have_selector('h1', text: 'Help')
 		end
 	end
 end
 
 describe "static_pages" do
+	describe "A Title Test" do
+		it "should have the right title" do
+	  		visit '/static_pages/help'
+  			page.should have_selector('title',
+                    text: "| Help")
+  		end
+  	end
+end
+
+describe "static_pages" do
 	describe "About Page" do
-		it "should have the content 'About Us'" do
+		it "should have the h1 'About Us'" do
 			visit '/static_pages/about'
-			page.should have_content('About Us')
+			page.should have_selector("h1", text:'About Us')
 		end
 	end
 end
+
+describe "static_pages" do
+	describe "A Title Test" do
+		it "should have the right title" do
+	  		visit '/static_pages/about'
+  			page.should have_selector('title',
+                    text: " | About Us")
+  		end
+  	end
+end
+
